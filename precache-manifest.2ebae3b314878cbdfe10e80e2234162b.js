08d4f4f623e4052e3f8b4632465dafb9c469e0a0
thongnguyen28101994 <nguyenthanhminhthong@gmail.com>
2022-06-20 13:36:08 +0700

    Updates

---
+++ precache-manifest.2ebae3b314878cbdfe10e80e2234162b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b116c02e02b39ae0e88e96b93c61fd24",
+    "url": "/dynamic_form/index.html"
+  },
+  {
+    "revision": "be5e84ad47b5c84dee3b",
+    "url": "/dynamic_form/static/css/2.b42b0c75.chunk.css"
+  },
+  {
+    "revision": "2d8d1ae5e978372c7389",
+    "url": "/dynamic_form/static/css/main.225db515.chunk.css"
+  },
+  {
+    "revision": "be5e84ad47b5c84dee3b",
+    "url": "/dynamic_form/static/js/2.af6eb056.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dynamic_form/static/js/2.af6eb056.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d8d1ae5e978372c7389",
+    "url": "/dynamic_form/static/js/main.5e5aaebc.chunk.js"
+  },
+  {
+    "revision": "909b9c61a53a82fe66eb",
+    "url": "/dynamic_form/static/js/runtime-main.8d1414c9.js"
+  },
+  {
+    "revision": "7e31e704c31f123c64e4c598e392901e",
+    "url": "/dynamic_form/static/media/logo_so.7e31e704.png"
+  }
+]);
